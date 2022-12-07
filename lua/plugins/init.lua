6c1c19dd54d5b260bb96b67f88d85fa6734cbaf3
xRyymm <rzero69@proton.me>
2022-12-07 10:54:44 -0600

    update catppuccin theme

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -209,7 +209,6 @@ return packer.startup(function(use)
   -- Themes
   use {
     "catppuccin/nvim",
-    commit = "df1b7ff",
     as = "catppuccin",
     event = "UIEnter",
     run = ":CatppuccinCompile",
